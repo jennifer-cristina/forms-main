0a18f0121672daf1cd1af7a3408a54eae95dbc46
Jennifer Cristina <cristin4.jennifer@gmail.com>
2024-10-08 22:16:21 -0300

    feat: formulários orientados a templates

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../services/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,9 +10,30 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private consultaCepService: ConsultaCepService) { }
 
   ngOnInit(): void {
+  }
+
+  consultaCep(ev: any, f: NgForm) {
+    const cep = ev.target.value;
+
+    if (cep !== '') {
+      this.consultaCepService.getConsultaCep(cep).subscribe(resultado => {
+        console.log(resultado)
+        this.populandoEndereco(resultado, f);
+      });
+    }
+  }
+
+  populandoEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade, 
+      estado: dados.uf
+    })
   }
 
   cadastrar(form: NgForm) {
